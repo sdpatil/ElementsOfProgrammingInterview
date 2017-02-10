952052060f9f4021d42aae0f03d1c1545a4579f5
sunilpatil <sunilpatil@maprtech.com>
2017-02-10 11:12:51 -0800

    Tries DataStructure related changes

---
+++ src/main/java/com/alg/misc/StringST.java
@@ -0,0 +1,50 @@
+package com.alg.misc;
+
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Created by spatil30 on 2/7/2017.
+ */
+public class StringST<Value> {
+
+    public void put(String key, Value value){
+
+    }
+
+    public Value get(String key){
+        return null;
+    }
+
+    public void delete(String key){
+
+    }
+
+    public boolean contains(String key){
+        return false;
+    }
+
+    public boolean isEmpty(){
+        return true;
+    }
+
+    public String logestPrefixOf(String key){
+        return null;
+    }
+
+    public Iterable<String> keysWithPrefix(String s){
+        return null;
+    }
+
+    public Iterable<String> keysThatMatch(String s){
+        return null;
+    }
+
+    public int size(){
+        return 0;
+    }
+
+    public Iterable<String> keys(){
+        return null;
+    }
+}
---
+++ src/test/java/alg/misc/TriesSTTest.java
@@ -0,0 +1,11 @@
+package alg.misc;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by spatil30 on 2/7/2017.
+ */
+public class TriesSTTest {
+
+}
